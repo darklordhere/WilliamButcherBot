3a7703a036d6e814afd9d8fd65c309045dfa0c0a
darklordhere <89247293+darklordhere@users.noreply.github.com>
2021-08-23 14:12:08 +0530

    Update __main__.py

--- wbb/__main__.py
+++ wbb/__main__.py
@@ -135,7 +135,7 @@ home_keyboard_pm = InlineKeyboardMarkup(
 )
 
 home_text_pm = (
-    f"Hey there! My name is {BOT_NAME}( https://telegra.ph/file/a7a3859d6ceea7dae9a3c.jpg ). I can manage your "
+    f"Hey there! My name is [ Makise Bot ]( https://telegra.ph/file/a7a3859d6ceea7dae9a3c.jpg ). I can manage your "
     + "group with lots of useful features, feel free to "
     + "add me to your group."
 )
